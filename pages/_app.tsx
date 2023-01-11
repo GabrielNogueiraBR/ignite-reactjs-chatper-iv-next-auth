8471f4db9b7fdaed7f4e2c43202a2a408c434940
Gabriel Nogueira <gabrielnoog7@gmail.com>
2023-01-11 12:45:33 -0300

    feat: creating home page and auth context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { AuthProvider } from "../context/AuthContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Head>
+        <title>Next Auth Project</title>
+        <meta
+          name="description"
+          content="Repository created to save next project with authentication develop during the React course on Rocketseat."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
